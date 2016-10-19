62bc7f2993fdcf2a82bf0274da700f012d17080d
Cory Westerfield <cory.westerfield@gmail.com>
2016-10-19 09:18:29 -0600

    Add additional check for username and password

--- handlers.go
+++ handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -16,6 +17,23 @@ func HelloUserHandler(w http.ResponseWriter, r *http.Request) {
 
 	s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
 	if len(s) != 2 {
+		http.Error(w, "Not authorized", 401)
+		return
+	}
+
+	b, err := base64.StdEncoding.DecodeString(s[1])
+	if err != nil {
+		http.Error(w, err.Error(), 401)
+		return
+	}
+
+	pair := strings.SplitN(string(b), ":", 2)
+	if len(pair) != 2 {
+		http.Error(w, "Not authorized", 401)
+		return
+	}
+
+	if pair[0] != "username" || pair[1] != "password" {
 		http.Error(w, "Not authorized", 401)
 		return
 	}
